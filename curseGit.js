4dd056792d56068ce5e5405b5124085effcd381f
Lucian Tavares <lucian.sistemas.sc@gmail.com>
2021-11-19 17:18:15 -0300

    Include message about the course

--- curseGit.js
+++ curseGit.js
@@ -1,4 +1,5 @@
 let courseGit = 'Course Git'
 let contend = 'Course for beginner in Git'
+let message = 'Very complete course'
 
-console.log(courseGit, contend)
+console.log(courseGit, contend, message)
