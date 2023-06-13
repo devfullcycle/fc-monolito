2495cd2d77ea224638a81db8456f435e55ca6ce4
Jonas de Jesus Américo <jjsamerico@gmail.com>
2023-06-13 06:15:09 -0300

    Ajuste

--- src/modules/product-adm/facade/product-adm.facade.spec.ts
+++ src/modules/product-adm/facade/product-adm.facade.spec.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize-typescript";
 import ProductAdmFacadeFactory from "../factory/facade.factory";
-import { ProductModel } from "../repository/product.model";
+import ProductModel from "../repository/product.model";
 
 describe("ProductAdmFacade test", () => {
   let sequelize: Sequelize;
--- src/modules/product-adm/repository/product.repository.spec.ts
+++ src/modules/product-adm/repository/product.repository.spec.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize-typescript";
 import Id from "../../@shared/domain/value-object/id.value-object";
 import Product from "../domain/product.entity";
-import { ProductModel } from "./product.model";
+import ProductModel from "./product.model";
 import ProductRepository from "./product.repository";
 
 describe("ProductRepository test", () => {
